35f50ef55d5cc68c651b67ecb7e3e4ac7bb6571e
ayanoyokoyama <ayano.yokoyama@me.com>
2015-05-27 13:07:54 +0200

    added nav & modified index pg img

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   resources :products
-  get 'static_pages/about'
 
   get 'static_pages/contact'
 
   get 'static_pages/index'
+
+  get 'static-pages/about'
 
 
   # The priority is based upon order of creation: first created -> highest priority.
