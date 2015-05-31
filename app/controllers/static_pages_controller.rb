b350539d048d4fa2e208d23e8d72b0374599e8e8
ayanoyokoyama <ayano.yokoyama@me.com>
2015-05-31 11:14:44 +0200

    updated email setting & environment.rb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -13,5 +13,11 @@ def thank_you
 	ActionMailer::Base.mail(:from => @email, :to => 'ayano.yokoyama@me.com', :subject => ' #@name sent you a message!', :body => @message).deliver
 end
 
+def thank_you
+	@name = params[:name]
+	@email = params[:email]
+	@message = params[:message]
+	UserMailer.contact_form(@email, @name, @message).deliver
+end
   
 end
