53a052d17a14594c777332b48f6ff9e7c5820e5d
ayanoyokoyama <ayano.yokoyama@me.com>
2015-05-31 19:25:04 +0200

    added new feature branch product_review

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,12 +4,14 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-    #   user ||= User.new # guest user (not logged in)
+    user ||= User.new # guest user (not logged in)
     #   if user.admin?
-    #     can :manage, :all
+    can :manage, User, id: user.id
     #   else
     #     can :read, :all
-    #   end
+    end 
+    
+  end
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
