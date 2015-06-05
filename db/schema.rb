1b43c6b046e5f9dcfbb26a77e1d66ca6e46157e6
ayanoyokoyama <ayano.yokoyama@me.com>
2015-06-05 22:35:03 +0200

    modified schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150531104343) do
+ActiveRecord::Schema.define(version: 20150605202426) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
