342b8847432621ee8df3311e06a33bff903c9db6
ayanoyokoyama <ayano.yokoyama@me.com>
2015-07-03 19:04:56 +0200

    added pagination

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ gem 'devise'
 
 gem 'cancancan', '~> 1.10'
 
+gem 'will_paginate'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.1'
 # Use sqlite3 as the database for Active Record
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,6 +19,7 @@ class ProductsController < ApplicationController
   # GET /products/1.json
   def show
     @comments = @product.comments.all.order("created_at DESC")
+    @comments = @product.comments.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 2)
   end
 
   # GET /products/new
