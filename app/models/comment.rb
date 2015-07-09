d87085b27a673700853b862a1002af073faf9959
ayanoyokoyama <ayano.yokoyama@me.com>
2015-07-09 19:45:02 +0200

    modified admin function

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
-
   validates :body, presence: true
-  validates :rating, presence: true, numericality: { only_integer: true }
+  validates :user, presence: true
+  validates :product, presence: true
+  validates :rating, numericality: { only_integer: true }
 end
