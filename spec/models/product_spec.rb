8401bee83a956d5b241a575ac7e893844b4de980
ayanoyokoyama <ayano.yokoyama@me.com>
2015-11-24 16:56:53 +0100

    deleted spec files and modified

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -10,41 +10,13 @@ describe Product do
 		@product.comments.create(:rating => 5, :user => @user, :body => "Perfect cocktail!")
 	end
 	
-end
 
 it "returns the average rating of all comments" do
 end
 
 it "should the averege rating to eq 3"
 
-describe User do
 
-	context "first and last name presnt" do
-
-		before { @user = User.create(first_name: "marie", last_name: "yokoyama") }
-
-		it "should return first and last"
-			expect{@person.full_name}.to eq "marie yokoyama"
-		end
-	end
-
-	context "last name missing" do
-
-		before { @user = User.create(first_name: "John") }
-
-		it "should return first name only" do
-			expect(@person.full_name).to eq "marie"
-		end
-	end
-
-	context "no names present" do
-
-		before { @person = Person.new() }
-
-		it "should return nothing" do
-			expect(@person.full_name).to eq ""
-		end
-	end
 end
 
 
