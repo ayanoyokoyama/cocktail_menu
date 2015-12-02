6bce1b4139d45bc237c28ffbabda574816009a2d
ayanoyokoyama <ayano.yokoyama@me.com>
2015-12-02 15:31:54 +0100

    modified product_spec & added user_spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,14 +6,15 @@ describe Product do
 
 		before do
 			#here you put your code to generate test content
+			@user = User.create(:email => 'marie@gmail.com', :password => 'abcdefg')
 			@product = Product.create(:name => 'mojito')
 			@product.comments.create(:rating => 1, :user => @user, :body => "Awful cocktail!")
 			@product.comments.create(:rating => 3, :user => @user, :body => "Okay cocktail.")
 			@product.comments.create(:rating => 5, :user => @user, :body => "Perfect cocktail!")
 		end
-
+		
 		it "returns the average rating of all comments" do
-			expect(@product.average_rating).to eq "3"
+			expect(@product.average_rating).to eq 3
 		end
 	end
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe User do
+	it "is valid with a valid email and password" do
+		
+	end
+
+	it "is invalid without a valid email address" do
+	
+	end
+end
