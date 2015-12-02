b7b5973e5a60272e97673cd42ad9560c6b82544e
ayanoyokoyama <ayano.yokoyama@me.com>
2015-12-02 03:06:06 +0100

    modified spec file

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,20 +2,23 @@ require 'rails_helper'
 
 describe Product do
 
-	before do
-		#here you put your code to generate test content
-		@product = Product.create(:name => 'mojito')
-		@product.comments.create(:rating => 1, :user => @user, :body => "Awful cocktail!")
-		@product.comments.create(:rating => 3, :user => @user, :body => "Okay cocktail.")
-		@product.comments.create(:rating => 5, :user => @user, :body => "Perfect cocktail!")
+	context "when product has comments" do
+
+		before do
+			#here you put your code to generate test content
+			@product = Product.create(:name => 'mojito')
+			@product.comments.create(:rating => 1, :user => @user, :body => "Awful cocktail!")
+			@product.comments.create(:rating => 3, :user => @user, :body => "Okay cocktail.")
+			@product.comments.create(:rating => 5, :user => @user, :body => "Perfect cocktail!")
+		end
+
+		it "returns the average rating of all comments" do
+			expect(@product.average_rating).to eq "3"
+		end
+
 	end
 
-it "returns the average rating of all comments" do
-	expect (@product.rating).to eq "3"
-end
-
-it "should the averege rating to eq 3" do
-end
-
+		it "should the averege rating to eq 3" do
+		end
 end
 	
