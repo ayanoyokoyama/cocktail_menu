5ecc7e0565f5d881c008168fcbcff912794cd077
ayanoyokoyama <ayano.yokoyama@me.com>
2015-12-10 23:54:02 +0900

    added functional tests

---
+++ spec/controllers/user_controller_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+
+describe UserController, :type => :controller do
+	
+	before do
+		@user = User.create(:email => "test@example.com", :password => "genericpassword")
+		@user2 = User.create(:email => "test2@exaple.com", :password => "genericpassword2")
+	end
+
+	describe "GET #show" do
+		context "User is logged in" do
+			before do
+				sign_in @user
+			end
+			it "shows the correct user details" do
+				get :show, id: @user.id
+				expect(assigns(:user)).to eq @user
+			end
+			it "redirecrs to root path if user tries to access other users show page" do
+				get:show, id: @user2.id
+				expect(response).to redirect_to(root_path)
+			end
+	end
+
+	context "No user is logged in" do
+		it "redirects to login" do
+			get :show, id: @user.id
+			expect(response).to redirect_to(root_path)
+		end
+	end
+end
+
+end
